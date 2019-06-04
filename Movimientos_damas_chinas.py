40a0a2cd54f07d42a465460430b87107352d06e3
MarlonGamba17 <mgambav18183@universidadean.edu.co>
2019-06-03 20:08:13 -0500

    Commit inicial damas chinas: desarrollo de peon blanco

---
+++ Movimientos_damas_chinas.py
@@ -0,0 +1,131 @@
+#PENDIENTES:
+# 1. Pruebas
+# 2. Regla de matar consecutivamente
+# 3. Movimientos de Reina
+
+tablero = [
+[' ', 'p', ' ', 'p', ' ', 'p', ' ', 'p'],
+['p', ' ', 'p', ' ', 'p', ' ', 'p', ' '],
+[' ', 'p', ' ', 'p', ' ', 'p', ' ', 'p'],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+['P', ' ', 'P', ' ', 'P', ' ', 'P', ' '],
+[' ', 'P', ' ', 'P', ' ', 'P', ' ', 'P'],
+['P', ' ', 'P', ' ', 'P', ' ', 'P', ' ']
+]
+
+def tablero_a_cadena(tablero):
+    """
+    (list of str) -> str
+    recibimos un tablero o diccionario y devolvemos una cadena
+    :param tablero: el tablero con la posicionde cada ficha
+    :return: dict el tablero con la nueva posicion de las fichas
+    """
+
+    cadena = ""
+    for fila in tablero:
+        cadena += str(fila) + "\n"
+    return cadena
+
+def obtener_nombre_pieza(simbolo):
+    """
+    (str) -> str
+    >>> obtener_nombre_pieza('p')
+    'Peon fichas blancas'
+    >>> obtener_nombre_pieza('P')
+    'Peon Fiches Negras'
+    Retorna el nombre de la pieza del ajedrez dado su simbolo
+    :param simbolo: la representacion de la pieza segun el enunciado
+    :return: El nombre y color de la pieza
+    """
+    tipo = 'Negras'
+    if simbolo.islower():
+        tipo = 'blancas'
+    retorno = simbolo.lower()
+    if retorno == 'p':
+        return 'Peon '+tipo
+    if retorno == 'r':
+        return 'Reina '+tipo
+    else:
+        return 'No es una pieza'
+
+def mover_peon(tablero, x_inicial, y_inicial, x_final, y_final):
+    '''
+    ([][],int,int,int,int)-> [][]: tablero resultante del movimiento de un peon.
+    :param tablero: [][]: matriz con la posicion de las fichas
+    :param x_inicial: int: entero indicando posicion en x inicial
+    :param y_inicial: int: entero indicando posicion en y inicial
+    :param x_final: int: entero indicando posicion en x final
+    :param y_final: int: entero indicando posicion en y final
+    :return: [][] tablero resultante
+    '''
+
+    if (0 < x_final <= 7) and (0 < y_final <= 7):
+        esPeon = tablero[x_inicial][y_inicial] in 'pP'
+        deltaX = abs(x_final - x_inicial)
+        deltaY = abs(y_final - y_inicial)
+        ficha = tablero[x_inicial][y_inicial]
+        color_ficha = tablero[x_inicial][y_inicial].islower()
+
+        if (esPeon):
+            if (color_ficha == True):
+                if (x_final > x_inicial):
+                    if (deltaY == deltaX) and ((x_final != x_inicial) and (y_final != y_inicial)):
+                        if ((deltaX == 1) and (deltaY == 1)):
+                            if tablero[x_final][y_final] == ' ':
+                                tablero[x_final][y_final] = ficha
+                                tablero[x_inicial][y_inicial] = ' '
+
+                                print( tablero_a_cadena(tablero) )
+                            else:
+                                print("La posicion final esta ocupada")
+                        elif ((deltaX == 2) and (deltaY == 2)):
+                            # Matar hacia la izquierda
+                            if (y_final < y_inicial):
+                                ficha_a_comer = tablero[x_final - 1][y_final + 1]
+                                color_ficha_a_comer = ficha_a_comer.islower()
+                                if (ficha_a_comer != ' ') and (color_ficha != color_ficha_a_comer):
+                                    tablero[x_final - 1][y_final + 1] = ' '
+                                    tablero[x_final][y_final] = ficha
+                                    tablero[x_inicial][y_inicial] = ' '
+
+                                    print( tablero_a_cadena(tablero) )
+                                else:
+                                    print("Movimiento invalido: no se puede mover 2 posiciones sin comer ficha enemiga")
+                            # Matar hacia la derecha
+                            elif (y_final > y_inicial):
+                                ficha_a_comer = tablero[x_final - 1][y_final - 1]
+                                color_ficha_a_comer = ficha_a_comer.islower()
+                                if (ficha_a_comer != ' ') and (color_ficha != color_ficha_a_comer):
+                                    tablero[x_final - 1][y_final - 1] = ' '
+                                    tablero[x_final][y_final] = ficha
+                                    tablero[x_inicial][y_inicial] = ' '
+
+                                    print( tablero_a_cadena(tablero) )
+                                    # TODO
+                                    # reclamar_reina(x_final, y_final)
+
+
+                                else:
+                                    print("Movimiento invalido: no se puede mover 2 posiciones sin comer ficha enemiga")
+
+                        else:
+                            print("Movimiento invalido: solo se puede avanzar 1 posicion, o 2 posiciones si se va a comer una ficha")
+                    else:
+                        print("Movimiento invalido: solo se puede mover en diagonal")
+                else:
+                    print("La ficha solo se mueve hacia adelante")
+            if (color_ficha == False):
+                if (x_final < x_inicial):
+                    pass
+                else:
+                    print("La ficha solo se mueve hacia adelante")
+        else:
+            print('Posición final fuera del tablero.')
+
+        return tablero
+    else:
+        print("La ficha a mover no es un peon.")
+
+if __name__ == '__main__':
+    mover_peon(tablero, 2,1,4,3)
